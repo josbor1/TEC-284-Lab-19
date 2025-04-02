cc60bfea31f76c0ab4c04deac5527c7b922e5cff
josbor1 <123982835+josbor1@users.noreply.github.com>
2025-04-02 16:41:44 -0500

    Create Lab 19.ino

---
+++ Lab 19.ino
@@ -0,0 +1,160 @@
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example, rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second.
+*/
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint8_t txValue = 0;
+
+// Create motor shield object
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);  // Right motor
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);   // Left motor
+
+// UUIDs for BLE service and characteristics
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+  }
+};
+// Movement functions
+void moveStraight() {
+  rightMotor->run(BACKWARD);
+  leftMotor->run(BACKWARD);
+}
+
+void moveBackwards() {
+  rightMotor->run(FORWARD);
+  leftMotor->run(FORWARD);
+}
+
+void steerLeft() {
+  rightMotor->run(BACKWARD);
+  leftMotor->run(FORWARD);
+}
+
+void steerRight() {
+  rightMotor->run(FORWARD);
+  leftMotor->run(BACKWARD);
+}
+
+void stop() {
+  rightMotor->run(RELEASE);
+  leftMotor->run(RELEASE);
+}
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String rxValue = pCharacteristic->getValue();
+
+    if (rxValue.length() > 0) {
+      Serial.println("*********");
+      Serial.print("Received Value: ");
+      for (int i = 0; i < rxValue.length(); i++) {
+        Serial.print(rxValue[i]);
+      }
+      Serial.println();
+      Serial.println("*********");
+
+      if (rxValue == "!B516") {
+        moveStraight();
+      } else if (rxValue == "!B615") {
+        moveBackwards();
+      } else if (rxValue == "!B714") {
+        steerLeft();
+      } else if (rxValue == "!B813") {
+        steerRight();
+      } else {
+        stop();
+      }
+    }
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+  AFMS.begin();
+
+  rightMotor->setSpeed(150);
+  leftMotor->setSpeed(150);
+
+  // Create the BLE Device
+  BLEDevice::init("Osborne's Race Car");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create BLE Characteristics
+  pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting for a client connection to notify...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    pTxCharacteristic->setValue(&txValue, 1);
+    pTxCharacteristic->notify();
+    txValue++;
+    delay(10);  // Avoid congestion in the BLE stack
+  }
+
+  // Handling disconnection and reconnection
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500);  // Allow the BLE stack to reset
+    pServer->startAdvertising();
+    Serial.println("Start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+  }
+}
